9caebf74b4c0986ac0adeac5b8ff7d119035679e
khaoula102 <khaoularadi102@gmail.com>
2023-03-16 07:39:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n")
+#include<stdio.h>
+main(){
+    puts("\"Programming is like building a multilingual puzzle\n")
+    return(0)
+}
