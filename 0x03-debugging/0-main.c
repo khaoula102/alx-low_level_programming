4bee33ffbbbaa05bfba2d8bfd8a7341e5fa941cc
khaoula102 <khaoularadi102@gmail.com>
2023-03-23 17:20:44 -0700

    all

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Tests function that prints if integer is positive or negative
+ * main - Test function for positive or negative
  * Return: 0
  */
 
@@ -11,5 +11,6 @@ int i;
 
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
