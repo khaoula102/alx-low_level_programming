c3538068fabe94d386fe49a562337bff6622fba7
khaoula102 <khaoularadi102@gmail.com>
2023-04-03 15:17:24 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* _strstr - Entry point
+* @haystack: input
+* @needle: input
+* Return: Always 0 (Success)
+*/
+char *_strstr(char *h, char *n)
+{
+for (; *h != '\0'; h++)
+{
+char *l = h;
+char *p = n;
+
+while (*l == *p && *p != '\0')
+{
+l++;
+p++;
+}
+
+if (*p == '\0')
+return (h);
+}
+
+return (0);
+}
+
