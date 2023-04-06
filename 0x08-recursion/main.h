9caafe2b31f44ed3ace4fbb921439ae27b8f4e13
khaoula102 <khaoularadi102@gmail.com>
2023-04-06 12:40:09 -0700

    all

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,7 +5,7 @@ void _puts_recursion(char *a);
 int _putchar(char a);
 void _print_rev_recursion(char *a);
 int _strlen_recursion(char *a);
-int factorial(int a);
+int factorial(int t);
 int _pow_recursion(int s, int t);
 int _sqrt_recursion(int n);
 int is_prime_number(int a);
